24bfefa91f8b45255b0432e663acd0887c8219a6
Kaixi26 <jorgejg575926@gmail.com>
2020-04-22 14:16:00 +0100

    oi

--- src/filetree.h
+++ src/filetree.h
@@ -26,17 +26,29 @@ struct s_ftree {
     Filetree** children;
 };
 
+/* Allocate space for the filetree */
 Filetree* filetree_init();
+/* Makes a directory in the current root */
 int filetree_mkdir(Filetree*, const char*);
+/* Creates a new file in the current directory */
 int filetree_mkfile(Filetree*, const char*);
-Filetree* filetree_cd(Filetree*, const char* name);
+/* Changes to a child directory and returns it */
+Filetree* filetree_cd(Filetree*, const char*);
+/* Opens a file and returns it */
 Filetree* filetree_open(Filetree*, const char* name);
+/* Appends a string to the file */
 void filetree_append(Filetree*, const char* text);
+/* Moves up 'int' directories */
 Filetree* filetree_moveup(Filetree*, int);
+/* Moves filetree to main root */
 Filetree* filetree_toroot(Filetree*);
+/* Opens a file given the full path from current directory */
 Filetree* filetree_search_file(Filetree*, char* path);
+/* Prints the filetree in a human readable way */
 void filetree_print(Filetree*);
+/* Writes the filetree to the system, will exit program in case of failure */
 void filetree_build(Filetree*);
+/* Frees the space allocated for the filetree */
 void filetree_free(Filetree*);
 
 #endif
--- src/metadata.h
+++ src/metadata.h
@@ -12,8 +12,13 @@ typedef struct {
 
 /* Allocates memory needed for structure and returns pointer to it */
 Metadata* metadata_init();
+/* Fetches the value associated with a key */
 const char* metadata_get(Metadata*, const char* key);
+/* Inserts a key value pair into the structure */
 void metadata_put(Metadata*, const char* key, const char* value);
+/* Prints the structure in a humanly readable way */
 void metadata_print(Metadata*);
+/* substitutes all the ocurring {%key%} with the corresponding value */
 Stringbuilder* metadata_substitute(Metadata* m, char* str);
+/* Deallocates the memory for the structure and all that depends on it */
 void metadata_free(Metadata*);
